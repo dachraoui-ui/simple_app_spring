54783162f7ec382dd3dfa70bd5bd1de90fc91832
ahmed dacharoui <dachraouia193@gmail.com>
2024-07-03 10:52:04 +0100

    first commit

---
+++ src/test/java/org/newapp/newapp/NewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.newapp.newapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
