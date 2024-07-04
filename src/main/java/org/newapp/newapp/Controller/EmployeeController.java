dec2799555b1f9e18e8bd386d414b9ef7d604ca9
ahmed dacharoui <dachraouia193@gmail.com>
2024-07-04 20:58:47 +0100

    fix some problems

---
+++ src/main/java/org/newapp/newapp/Controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package org.newapp.newapp.Controller;
+
+
+import org.newapp.newapp.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.ArrayList;
+import java.util.List;
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    public static List<Employee> employees = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10 ; i++) {
+            employees.add(new Employee(i, "Employee"+i, "Role"+i));
+        }
+    }
+    @GetMapping
+    public String getAllEmployees(Model model){
+        model.addAttribute("employees",model);
+        return "employees";
+    }
+}
---
+++ src/main/java/org/newapp/newapp/model/Employee.java
@@ -0,0 +1,18 @@
+package org.newapp.newapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Employee {
+    private String id;
+    private String name;
+    private String role;
+
+}
